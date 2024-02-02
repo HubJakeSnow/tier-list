ac659d8cc081aac9e677b84ad74fab130eb2b7e3
Jake Snow <jakesnow@gmx.at>
2024-02-02 19:22:00 +0100

    Add drag and drop functionality

--- tier-list/src/RankingContainer.js
+++ tier-list/src/RankingContainer.js
@@ -1,13 +1,23 @@
 import React from 'react'
 
 function RankingContainer({ clubData }) {
+  const dragStarted=(e,index)=>{
+    console.log("Drag started")
+    e.dataTransfer.setData("clubId", index)
+  }
+
   return (
     <div className='ranking-container'>
       {clubData.map((badgeURL, index) => (
         <img
+          className='ranking-images'
           key={index} 
+          id={index}
           src={badgeURL} 
           style={{ maxWidth: '80px' }}
+          alt='Images to be ranked' // Can this be changed so that the name of the club is read out?
+          draggable
+          onDragStart={(e)=>dragStarted(e,index)}
         />
       ))}
     </div>
--- tier-list/src/Row.js
+++ tier-list/src/Row.js
@@ -2,10 +2,33 @@ import React from 'react'
 import { FaCog } from 'react-icons/fa'
 
 const Row = ({ tier, tierColor, onEdit }) => {
+  const draggingOver=(e)=> {
+    e.preventDefault()
+    console.log("Dragging over now")
+  }
+
+  const dragDropped = (e) => {
+    e.preventDefault();
+  
+    let transferedClubId = e.dataTransfer.getData('clubId');
+    console.log(`You have dropped ${transferedClubId} in ${tier} tier`);
+  
+    // Get the image element based on the dragged clubId
+    const droppedImage = document.getElementById(transferedClubId);
+  
+    // Append the image to the image-container in this row
+    e.target.appendChild(droppedImage);
+  };
+  
+
   return (
     <div className="row-container">
       <div className="tier-box" style={{backgroundColor: tierColor}}>{tier}</div>
-      <div className="image-container"> {/* This is where the images will go */}</div>
+      <div
+        onDragOver={(e) => draggingOver(e)}
+        onDrop={(e)=>dragDropped(e)}
+        className="image-container">
+      </div>
       <div className="icon-box" onClick={onEdit}>
         <FaCog className="icon" style={{backgroundColor: 'black'}} size={40} />
       </div>
