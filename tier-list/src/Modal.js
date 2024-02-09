9b5ed495bc9b9d89077f5d2fd3d003f6ae308ed9
Jake Snow <jakesnow@gmx.at>
2024-02-09 13:40:49 +0100

    Update styling on Modal

--- tier-list/src/Modal.js
+++ tier-list/src/Modal.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import EditTierForm from './EditTierForm'
 
 function Modal({ toggleVisibility, onSubmit }) {
+
+  useEffect(() => {
+    document.body.classList.add('modal-open');
+    return () => {
+      document.body.classList.remove('modal-open');
+    };
+  }, []);
+
   return (
     <div id='modal'>
       <EditTierForm toggleVisibility={toggleVisibility} onSubmit={onSubmit} />
